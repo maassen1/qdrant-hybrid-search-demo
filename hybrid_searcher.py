8c64c1eb895b3a0a0530d0730b52e9e5bd8335d1
maassen1 <99384259+maassen1@users.noreply.github.com>
2024-11-27 15:23:00 -0800

    Better documentation

--- hybrid_searcher.py
+++ hybrid_searcher.py
@@ -4,7 +4,7 @@ from qdrant_client import QdrantClient
 class HybridSearcher:
     DENSE_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
     SPARSE_MODEL = "prithivida/Splade_PP_en_v1"
-    def __init__(self, collection_name, url, api_key):
+    def __init__(self, collection_name: str, url: str, api_key: str):
         self.collection_name = collection_name
         self.url = url
         self.api_key = api_key
@@ -14,13 +14,13 @@ class HybridSearcher:
         # comment this line to use dense vectors only
         self.qdrant_client.set_sparse_model(self.SPARSE_MODEL)
 
-    def search(self, text: str, query_filter):
+    def search(self, text: str, query_filter: str) -> list:
         search_result = self.qdrant_client.query(
             collection_name=self.collection_name,
             query_text=text,
             query_filter=query_filter,
             limit=5,
         )
-        
+
         metadata = [hit.metadata for hit in search_result]
         return metadata
